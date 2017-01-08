987e18dfc7f6e2cc201e61e69028109c2894a47a
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-01-08 00:42:09 -0500

    save registers in mongoDB

--- src/backend/api.ts
+++ src/backend/api.ts
@@ -1,14 +1,18 @@
 import * as express from 'express';
+import { Logro } from './models/logro';
 
 const router = express.Router();
 
 router
   .route('/logros')
   .post((req, res) => {
-    console.log(req.body);
-    res.json({
-      demo: 'Test'
+    const logro = new Logro({
+      title: req.body.title,
+      description: req.body.description,
+      author: req.body.author
     });
+
+    logro.save().then((doc) => res.json(doc));
   });
 
 export const RouterApi = router;
--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,10 @@ import * as cookieParser from 'cookie-parser';
 import * as morgan from 'morgan';
 import * as compression from 'compression';
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/logros');
+
 // Angular 2
 import { enableProdMode } from '@angular/core';
 // Angular 2 Universal
