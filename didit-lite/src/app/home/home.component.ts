60efc2f0d7e86d8c9e1af33c06d3df9cfd02fe3b
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-01-07 21:36:35 -0500

    show data for a component

--- didit-lite/src/app/home/home.component.ts
+++ didit-lite/src/app/home/home.component.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  logros: ILogro[];
+  title: string = 'Bienvenido';
 
   constructor() { }
 
   ngOnInit() {
+    this.logros = this.getLogros();
   }
 
+  getLogros() : ILogro[] {
+    return [
+      {
+        id: 1,
+        title: 'Logré algo muy interesante',
+        description: 'Bacon ipsum dolor amet venison spare ribs strip steak bacon rump filet mignon pork chop kielbasa shankle tongue hamburger shank short loin chuck landjaeger. Flank kielbasa doner, pastrami biltong ribeye pancetta drumstick leberkas frankfurter pork belly rump. Tenderloin pork tail meatloaf frankfurter rump swine corned beef tri-tip. Pork loin shoulder alcatra spare ribs biltong shankle porchetta strip steak pork belly short loin swine capicola bacon'
+      },{
+        id: 2,
+        title: 'Logré otra cosa muy interesante',
+        description: 'Bacon ipsum dolor amet venison spare ribs strip steak bacon rump filet mignon pork chop kielbasa shankle tongue hamburger shank short loin chuck landjaeger. Flank kielbasa doner, pastrami biltong ribeye pancetta drumstick leberkas frankfurter pork belly rump. Tenderloin pork tail meatloaf frankfurter rump swine corned beef tri-tip. Pork loin shoulder alcatra spare ribs biltong shankle porchetta strip steak pork belly short loin swine capicola bacon'
+      },{
+        id: 3,
+        title: 'Logré algo aún más interesante',
+        description: 'Bacon ipsum dolor amet venison spare ribs strip steak bacon rump filet mignon pork chop kielbasa shankle tongue hamburger shank short loin chuck landjaeger. Flank kielbasa doner, pastrami biltong ribeye pancetta drumstick leberkas frankfurter pork belly rump. Tenderloin pork tail meatloaf frankfurter rump swine corned beef tri-tip. Pork loin shoulder alcatra spare ribs biltong shankle porchetta strip steak pork belly short loin swine capicola bacon'
+      }
+    ];
+  }
+}
+
+// id, title, description
+
+interface ILogro {
+  id: number;
+  title: string;
+  description?: string;
 }
