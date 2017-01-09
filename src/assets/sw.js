37a6595129e636f3de35e848c7522b4d13dc5a35
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-01-08 21:24:28 -0500

    save data in the cache

--- src/assets/sw.js
+++ src/assets/sw.js
@@ -1,1 +1,19 @@
-// Enviar este archivo
+// self => SW
+const CACHE_NAME = 'logros-facilito-v1';
+const CACHE_URLS = [
+  '/assets/indigo-pink.css'
+];
+
+// Es la 1ra vez que mandamos una SW
+// Se actualiza la SW
+self.addEventListener('install', (ev) => {
+  caches.open(CACHE_NAME)
+    .then((cache) => {
+      console.log("Cache abierto");
+      return cache.addAll(CACHE_URLS);
+    });
+});
+
+self.addEventListener('activate', () => {});
+
+self.addEventListener('fetch', () => {});
